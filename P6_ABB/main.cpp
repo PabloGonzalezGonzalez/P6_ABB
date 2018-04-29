52fa113e42cf5f9cf2263e9fe1c7fbdeac98eb9a
Pablo <alu0100887037@ull.edu.es>
2018-04-29 18:12:57 +0100

    Initial Commit

---
+++ P6_ABB/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  P6_ABB
+//
+//  Created by Pablo González González on 29/4/18.
+//  Copyright © 2018 Pablo González González. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
